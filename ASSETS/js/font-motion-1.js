4de6a815d72766040911d82843aedc5a850518d6
Sourabh Kumbhar <kumsou6@gmail.com>
2020-02-21 00:12:59 +0530

    modified navbar color, added typed js

--- ASSETS/js/font-motion-1.js
+++ ASSETS/js/font-motion-1.js
@@ -12,27 +12,42 @@ function myFunction(item, index) {
 }
 
 
-anime.timeline({loop: true})
-  .add({
-    targets: '.ml1 .letter',
-    scale: [0.3,1],
-    opacity: [0,1],
-    translateZ: 0,
-    easing: "easeOutExpo",
-    duration: 600,
-    delay: (el, i) => 70 * (i+1)
-  }).add({
-    targets: '.ml1 .line',
-    scaleX: [0,1],
-    opacity: [0.5,1],
-    easing: "easeOutExpo",
-    duration: 700,
-    offset: '-=875',
-    delay: (el, i, l) => 80 * (l - i)
-  }).add({
-    targets: '.ml1',
-    opacity: 0,
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 1000
-  });
+
+   
+      console.log('present')
+      anime.timeline({loop: 1})
+      .add({
+      targets: '.ml1 .letter',
+      scale: [0.3,1],
+      opacity: [0,1],
+      translateZ: 0,
+      easing: "easeOutExpo",
+      duration: 600,
+      delay: (el, i) => 70 * (i+1)
+      }).add({
+      targets: '.ml1 .line',
+      scaleX: [0,1],
+      opacity: [0.5,1],
+      easing: "easeOutExpo",
+      duration: 700,
+      offset: '-=875',
+      delay: (el, i, l) => 80 * (l - i)
+      }).add({
+      targets: '.ml1',
+      opacity: 1,
+      duration: 1000,
+      easing: "easeOutExpo",
+      delay: 1000
+      });
+    
+  
+
+  
+
+
+
+	
+
+
+ 
+
